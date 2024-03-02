8fcbf423c7bb9ef552210a32bdff4808642c7dcc
Walter Vargas <wvargas@extraspace.com>
2024-03-02 11:22:52 -0600

    Fixing template for failed checks

---
+++ tests/tester.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { htmlReport } from '../dist/bundle.js'
+import { check,group } from 'k6';
+
+export default function () {
+  group('demo', ()=>{
+    const res = http.get('http://test.k6.io/');
+    check(res, {
+      'is status 200': (r) => r.status === 200,
+      'failed': true !== true
+    });
+
+  })
+}
+
+export function handleSummary(data) {
+
+  return {
+    'summary.html': htmlReport(data),
+  }
+}
